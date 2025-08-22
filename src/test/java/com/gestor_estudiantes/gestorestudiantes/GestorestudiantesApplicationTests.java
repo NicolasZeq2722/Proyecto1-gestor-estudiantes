66b3b5d5062165dfcce05fd5a70cc0ca36387d11
Nicolas zequea <nicomansolov89@gmail.com>
2025-08-22 12:28:49 -0500

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/gestor_estudiantes/gestorestudiantes/GestorestudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor_estudiantes.gestorestudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorestudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
