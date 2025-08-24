8aea1c059707c2c8f3d80c4c9c85cce67d7414a5
Nicolas zequea <nicomansolov89@gmail.com>
2025-08-23 22:22:50 -0500

    Nuevos cambios 23/08/2025

--- src/main/java/services/StudentServices.java
+++ src/main/java/services/StudentServices.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface StudentServices {
     public Student findById(Long id);
+    public List<Student> findAll();
     public Student save(Student student);
     public void deleteById(Long id);
-    public List<Student> findAll();
 }
